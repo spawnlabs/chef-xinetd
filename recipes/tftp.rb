d108eb174d2f8322087564827fbdac39a1b8d757
Chris Doughty <chrisdoughty@gamestop.com>
2013-05-02 11:21:42 -0500

    Adding tftp support

---
+++ recipes/tftp.rb
@@ -0,0 +1,23 @@
+package "tftp" do
+  action :install
+end
+
+package "tftpd" do
+  action :install
+end
+
+template "/etc/xinetd.d/tftp" do
+  mode 00644
+  source "tftp.erb"
+  notifies :restart, "service[xinetd]"
+end
+
+directory "/var/lib/tftpboot" do
+  owner "root"
+  group "root"
+  mode 00777
+  action :create
+  notifies "execute[chmod -R 777 /var/lib/tftpboot]"
+end
+
+execute "chmod -R 777 /var/lib/tftpboot"
